b9b002e864df07634e3965eaf75c9b0520ad3e33
Rahul Dubey <111676105+Rahuldubey26@users.noreply.github.com>
2024-11-12 11:43:15 +0000

    Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//This is Node-js Multer
+const path=require('path');
+const express = require('express')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const app=express();
+const PORT=8000;
+
+app.set("view engine","ejs");
+app.set("views",path.resolve('./views'));
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
+    return res.render("homepage");
+});
+app.post('/upload',upload.single("profileImage"),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+
+    return res.redirect("/");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Startd at a PORT:8000`)
+});
