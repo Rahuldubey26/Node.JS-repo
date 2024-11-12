307730ee690ef3c34f7a0a10475b07ba353d836d
Rahul Dubey <111676105+Rahuldubey26@users.noreply.github.com>
2024-11-12 17:23:21 +0000

    final commit for multer

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 const path=require('path');
 const express = require('express')
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+     return cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      return cb(null,`${Date.now()}-${file.originalname}`);
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 const app=express();
 const PORT=8000;
